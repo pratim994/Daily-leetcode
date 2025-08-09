75fcc5bd94bdb9b8de1cffd7277ef32116d6b44e
pratim chakraborty <aryanpratim86@gmail.com>
2025-08-09 16:27:28 +0530

    Add files via upload
    My sincere apologies as I couldn't upload for a few weeks.

---
+++ bitshift.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+bool isAPowerOfTwo(int n ){
+    return n>0 && (n&(n-1))==0 ;
+
+}
+
+int main(){
+    int n;
+    cout<<"enter the number you wanna check";
+    cin>>n;
+
+   cout<< (isAPowerOfTwo(n)? "yes its a power of two" : "no its not power of two") << endl;
+
+
+    return 0;
+}
