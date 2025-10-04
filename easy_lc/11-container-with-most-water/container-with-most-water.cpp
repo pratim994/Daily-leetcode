708db1c1cae737c785a17a1d027e8bd718f03255
pratim chakraborty <aryanpratim86@gmail.com>
2025-10-04 10:25:25 +0530

    Time: 3 ms (35.32%) | Memory: 63.1 MB (12.56%) - LeetSync

---
+++ easy_lc/11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int i =0;
+        int j = h.size()-1;
+        int res = 0;
+        while(i<j){
+            res = max(res,(j-i)*(min(h[i], h[j])));
+            if(h[i]<h[j]) i++;
+            else j--;
+        }
+        return res;
+    }
+};
