deb7a91d85f1da565f0ff91e25fa0e84367716a4
pratim chakraborty <aryanpratim86@gmail.com>
2025-10-10 10:37:36 +0530

    Time: 78 ms (97.89%) | Memory: 152.7 MB (17.25%) - LeetSync

--- easy_lc/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ easy_lc/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
   constexpr int maximumEnergy(vector<int>& e, int k) {
+            ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
     int n = e.size();
     vector<int> dp(n+1);
     int ans  = INT_MIN;
