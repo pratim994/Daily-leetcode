d79aa8b238c3091e9b080110b29b04ccffeca11a
pratim chakraborty <aryanpratim86@gmail.com>
2025-10-18 07:44:18 +0530

    Time: 127 ms (41.32%) | Memory: 97.5 MB (98.74%) - LeetSync

---
+++ easy_lc/3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        int n = nums.size(), ans  =0;
+        sort(nums.begin(),nums.end() );
+        int prev = -1e9;
+        for(int i =0;i<n;i++){
+            int l = max(nums[i]-k, prev+1);
+            if(l<= nums[i]+k){
+                prev = l, ans++;
+            }
+        }
+        return ans;
+    }
+};
