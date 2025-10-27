7490c6a570707ca4bb6c3596239774e2f1f01e98
pratim chakraborty <aryanpratim86@gmail.com>
2025-10-27 10:35:24 +0530

    Time: 4 ms (75.72%) | Memory: 27.2 MB (84.17%) - LeetSync

--- easy_lc/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ easy_lc/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int> sd(bank.size(),0);
-        if(bank.empty()) return 0;
-        int row =0;
-        for(auto it : bank){
-            int count =0;
-            for(int i = 0;i<bank[0].length();i++){
-                if(it[i] == '1') count++;
-
-            }
-            sd[row++] = count;
+       int ans = 0, prev =0;
+       for(auto& row : bank){
+        int device = count(row.begin(), row.end(), '1');
+        if(device >0){
+            ans += device*prev;
+            prev =  device;
         }
-        vector<int> sdwz;
-        for(int i= 0;i<sd.size();i++){
-            if(sd[i]!= 0) sdwz.push_back(sd[i]);
-        }
-        int ans = 0;
-        if(sdwz.size()<=1) return ans;
-
-        for(int i = 0;i<sdwz.size()-1;i++){
-            ans += sdwz[i]*sdwz[i+1];
-        }
-        return ans;
+       }
+       return ans;
     }
 };
