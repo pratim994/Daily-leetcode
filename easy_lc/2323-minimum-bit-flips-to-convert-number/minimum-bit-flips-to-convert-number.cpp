78b9ee69e3111ec837244f763d04b8c92d8bae6e
pratim chakraborty <aryanpratim86@gmail.com>
2025-11-02 08:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.34%) - LeetSync

---
+++ easy_lc/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorR = start^goal;
+        int ans = 0;
+        while(xorR>0){
+            ans += xorR&1;
+            xorR>>=1;
+        }
+        return ans;
+    }
+};
