fb6e1cfbe9059c82ecb478052dd816ecb2d2379b
pratim chakraborty <aryanpratim86@gmail.com>
2025-11-02 07:59:46 +0530

    Time: 51 ms (84.40%) | Memory: 173.1 MB (63.30%) - LeetSync

---
+++ easy_lc/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& g, vector<vector<int>>& w) {
+      vector<vector<int>> grid(m, vector<int>(n,0));
+      for(const auto& guard : g){
+        grid[guard[0]][guard[1]] = 1;
+      }
+      for(const auto& wall : w){
+        grid[wall[0]][wall[1]] = 2;
+      }
+
+      auto mark_guarded = [&](int r , int c){
+        for(int row = r+1;row<m;row++){
+            if(grid[row][c] == 1 || grid[row][c] == 2) break;
+            grid[row][c] = 3;
+        }
+        for(int row = r-1;row>= 0;row--){
+            if(grid[row][c] == 1 || grid[row][c] == 2) break;
+            grid[row][c] = 3;
+        }
+        for(int col = c+1;col<n;col++){
+            if(grid[r][col] == 1 ||grid[r][col] == 2) break;
+            grid[r][col] =3;
+        }
+        for(int col = c-1;col>=0;col--){
+            if(grid[r][col] == 1 || grid[r][col] == 2) break;
+            grid[r][col] = 3;
+        }
+      };
+      for(const auto& guard :  g){
+        mark_guarded(guard[0], guard[1]);
+
+      }
+      int res =0;
+      for(const auto& row : grid){
+        for(int cell : row){
+            if(cell == 0) res++;
+        }
+      }
+      return res;
+    }
+};
