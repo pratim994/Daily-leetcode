ac3fe768a3b44349bcfb62472e19a549085f0e66
pratim chakraborty <aryanpratim86@gmail.com>
2026-02-01 21:55:32 +0530

    3263-DIVIDE-AN-ARRAY-INTO-SUBARRAYS-WITH-MINIMUM-COST-I Stats: Time: 0 ms (100%), Space: 30.7 MB (17.09%) - LeetHub

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/3263-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/3263-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -3,9 +3,7 @@ public:
     int minimumCost(vector<int>& nums) {
         
         int n = nums.size();
-
-     
-       std::sort(nums.begin()+1, nums.begin()+n);
+         std::sort(nums.begin()+1, nums.begin()+n);
       int  cost = nums[0] + nums[1]+nums[2];
         return cost;
     }
