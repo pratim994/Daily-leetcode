771198ce2832a1d9120334de28d8f52d0521b1e6
pratim chakraborty <aryanpratim86@gmail.com>
2026-03-01 13:15:17 +0530

    0137-SINGLE-NUMBER-II Stats: Time: 0 ms (100%), Space: 13.5 MB (25.06%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int , int> freqMap;
-        for(auto& n : nums){
-            freqMap[n]++;
-        }   
-        for(auto& i : freqMap)
-            if(i.second == 1){
-            return i.first;
-    
-                }
-                return 0;
+       int ones = 0, twos =0;
+       for(auto& i : nums){
+        ones ^= (i & ~twos);
+        twos ^= (i & ~ones);
+       }
+       return ones;
            }
 };
+
+//so this was a naive iterative solution solution 
+// O(2n) t.s and O(n) s.c.
+
+//now lets try bit manipulation
